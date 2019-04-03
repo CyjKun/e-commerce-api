d92f90355ced41c8ceb04c261f4bbd6b6d188070
CJ <lightningpatapon@gmail.com>
2019-04-03 10:47:03 +0800

    add auth0 authentication

--- src/main/kotlin/com/ecommerce/ecommerce/repository/ProductRepository.kt
+++ src/main/kotlin/com/ecommerce/ecommerce/repository/ProductRepository.kt
@@ -3,6 +3,4 @@ package com.ecommerce.ecommerce.repository
 import com.ecommerce.ecommerce.model.Product
 import org.springframework.data.repository.CrudRepository
 
-interface ProductRepository: CrudRepository<Product, Long>{
-
-}
+interface ProductRepository: CrudRepository<Product, Long>
