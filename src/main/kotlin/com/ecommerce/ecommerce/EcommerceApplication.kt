8d52209b6c4e701d44dcaf1e5874a63216b41091
CJ Baquiran <cj.baquiran@whitecloak.com>
2019-04-04 10:02:59 +0800

    add mysql dependency for database configuration

--- src/main/kotlin/com/ecommerce/ecommerce/EcommerceApplication.kt
+++ src/main/kotlin/com/ecommerce/ecommerce/EcommerceApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class EcommerceApplication
+open class EcommerceApplication
 
 fun main(args: Array<String>) {
 	runApplication<EcommerceApplication>(*args)
--- src/main/kotlin/com/ecommerce/ecommerce/controller/ProductController.kt
+++ src/main/kotlin/com/ecommerce/ecommerce/controller/ProductController.kt
@@ -2,35 +2,42 @@ package com.ecommerce.ecommerce.controller
 
 import com.ecommerce.ecommerce.model.Product
 import com.ecommerce.ecommerce.repository.ProductRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/products")
-class ProductController(val productRepository: ProductRepository){
+class ProductController(@Autowired private val productRepository: ProductRepository) {
     //CREATE
     @PostMapping
-    fun saveProduct(@RequestBody product: Product)
-            = productRepository.save(product)
+    fun saveProduct(@RequestBody product: Product): Product = productRepository.save(product)
 
     //READ
     @GetMapping
-    fun getAllProducts()
-            = productRepository.findAll()
+    fun getAllProducts(): List<Product> = productRepository.findAll()
 
     @GetMapping("/{id}")
-    fun getOneProduct(@PathVariable id: Long)
-            = productRepository.findById(id)
+    fun getOneProduct(@PathVariable id: Long): ResponseEntity<Product> = productRepository.findById(id).map {
+        ResponseEntity.ok(it)
+    }.orElse(ResponseEntity.notFound().build())
 
     //UPDATE
-    @PutMapping("/{id}")
-    fun updateProduct(@RequestParam id: Long, @RequestBody product: Product){
-        assert(product.id == id)
-        productRepository.save(product)
-    }
+    //TO TEST
+//    @PutMapping("/{id}")
+//    fun updateProduct(@PathVariable id: Long, @Valid @RequestBody product: Product): ResponseEntity<Product> = productRepository.findById(id).map {
+//        val newProduct = it.copy(name = product.name, description =  product.description)
+//        ResponseEntity.ok().body(productRepository.save(newProduct))
+//    }.orElse(ResponseEntity.notFound().build())
+
 
     //DELETE
     @DeleteMapping("/{id}")
-    fun deleteProduct(@RequestParam id: Long)
-            = productRepository.deleteById(id)
+    fun deleteProduct(@RequestParam id: Long): ResponseEntity<Void> = productRepository.findById(id).map{
+        productRepository.delete(it)
+        ResponseEntity<Void>(HttpStatus.OK)
+    }.orElse(ResponseEntity.notFound().build())
 
 }
