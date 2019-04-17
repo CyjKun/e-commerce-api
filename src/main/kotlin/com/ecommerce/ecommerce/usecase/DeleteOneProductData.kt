da3608066fad45747d9f171a9488b5b6dbbeb3c4
CJ Baquiran <cj.baquiran@whitecloak.com>
2019-04-17 11:45:47 +0800

    changes with exception handling

--- src/main/kotlin/com/ecommerce/ecommerce/usecase/DeleteOneProductData.kt
+++ src/main/kotlin/com/ecommerce/ecommerce/usecase/DeleteOneProductData.kt
@@ -2,15 +2,17 @@ package com.ecommerce.ecommerce.usecase
 
 import com.ecommerce.ecommerce.domain.ProductRepository
 import com.ecommerce.ecommerce.exception.ProductServiceException
+import com.ecommerce.ecommerce.infrastracture.database.ProductJpaRepository
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
 
 @Service
 @Transactional
-class DeleteOneProductData(val productRepository: ProductRepository) {
+class DeleteOneProductData(val productRepository: ProductRepository,
+                           val productJpaRepository: ProductJpaRepository) {
     fun deleteOne(id: Long) {
-        if(productRepository.findById(id).id != id)
-            throw ProductServiceException("No Products to display")
+        if(!productJpaRepository.existsById(id))
+            throw ProductServiceException("No Products with id: " + id)
 
         return productRepository.deleteById(id)
     }
--- src/main/kotlin/com/ecommerce/ecommerce/usecase/GetOneProductData.kt
+++ src/main/kotlin/com/ecommerce/ecommerce/usecase/GetOneProductData.kt
@@ -3,14 +3,16 @@ package com.ecommerce.ecommerce.usecase
 import com.ecommerce.ecommerce.domain.Product
 import com.ecommerce.ecommerce.domain.ProductRepository
 import com.ecommerce.ecommerce.exception.ProductServiceException
+import com.ecommerce.ecommerce.infrastracture.database.ProductJpaRepository
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
 
 @Service
 @Transactional
-class GetOneProductData(val productRepository: ProductRepository) {
+class GetOneProductData(val productRepository: ProductRepository,
+                        val productJpaRepository: ProductJpaRepository) {
     fun getOne(id: Long): Product {
-        if(productRepository.equals(id))
+        if(!productJpaRepository.existsById(id))
             throw ProductServiceException("No Products with id: " + id)
         return productRepository.findById(id)
     }
