a6a19ed13d3240d555fdaca3ebccebfaa291201d
CJ Baquiran <cj.baquiran@whitecloak.com>
2019-04-17 17:17:51 +0800

    new folder arrangement for accounts and product

---
+++ e-commerce-accounts/src/main/kotlin/com/ecommerce/ecommerceaccounts/ECommerceAccountsApplication.kt
@@ -0,0 +1,11 @@
+package com.ecommerce.ecommerceaccounts
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ECommerceAccountsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ECommerceAccountsApplication>(*args)
+}
